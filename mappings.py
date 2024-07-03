0ede0d385c6641723c51eefc7bf8a66b3b1b3a37
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-07-03 17:28:55 +0200

    works for english sense repo upload

--- mappings.py
+++ mappings.py
@@ -1,2 +1,2 @@
-upos_mapping = {'ADJ': '', 'NOUN': '', 'MWE': '', 'ADV': '', 'VERB': ''}
-lang_mapping = {'sl': ''}
+upos_mapping = {'ADJ': 'Q8', 'NOUN': 'Q2', 'MWE': 'Q13', 'ADV': 'Q9', 'VERB': 'Q7', 'PROPN': 'Q14'}
+lang_mapping = {'sl': 'Q5', 'en': 'Q6'}
--- sense_inventory_upload.py
+++ sense_inventory_upload.py
@@ -1,5 +1,6 @@
-import csv, re, json
+import csv, re, json, time
 import mappings
+import xwbi
 
 # from wikibaseintegrator import wbi_login, WikibaseIntegrator
 from wikibaseintegrator.datatypes.string import String
@@ -17,9 +18,16 @@ from wikibaseintegrator.entities import lexeme
 # from wikibaseintegrator.models.claims import Claims
 # from wikibaseintegrator.models.qualifiers import Qualifiers
 
-
-inventory_filename = 'sense_inventories/elexis-wsd-sl_sense-inventory.tsv'
-language_wikicode = 'sl'
+inventory_filename = 'sense_inventories/elexis-wsd-en_sense-inventory.tsv'
+language_wikicode = 'en'
+try:
+    with open(f'sense_inventories/{language_wikicode}-lexeme-mapping.csv', 'r') as mappingcsv:
+        donelist = mappingcsv.read().split('\n')
+        done_items = []
+        for row in donelist:
+            done_items.append(row.split('\t')[0])
+except:
+    done_items = []
 
 
 with open(inventory_filename) as tsvfile:
@@ -30,33 +38,63 @@ with open(inventory_filename) as tsvfile:
     for line in senses_data:
         line_count += 1
         print(f"\n[{line_count}] Now processing: {line}")
-        if line['lemma'] not in upload_data:
-            upload_data[line['lemma']] = {'upos': line['upos'], 'senses': {line['sense_id']: line}}
-            print(f"Found new lemma '{line['lemma']}', added sense {line['sense_id']}")
+        lempos = line['lemma']+'_'+line['upos']
+        if lempos not in upload_data:
+            upload_data[lempos] = {line['sense_id']: line}
+            print(f"Found new lempos '{lempos}', added sense {line['sense_id']}")
         else:
-            upload_data[line['lemma']]['senses'][line['sense_id']] = line
-            print(f"Found repeated lemma '{line['lemma']}', added sense {line['sense_id']}")
+            upload_data[lempos][line['sense_id']] = line
+            print(f"Found repeated lempos '{lempos}', added sense {line['sense_id']}")
     with open(inventory_filename.replace(".tsv", ".json"), 'w') as jsonfile:
         json.dump(upload_data, jsonfile, indent=2)
         print(f"Saved data to upload to JSON.")
 
-    for lemma in upload_data: # TODO: what if same lemma different POS??
-
-        wikibase_pos = mappings.upos_mapping[upload_data[lemma]['upos']]
+    count = 0
+    for lempos in upload_data:
+        count += 1
+        print(f"\n[{count}] Now uploading {lempos}...")
+        if lempos in done_items:
+            print(f"{lempos} was done in a previous run, skipped.")
+            continue
+        lemma = lempos.split("_")[0]
+        upos = lempos.split("_")[1]
+        wikibase_pos = mappings.upos_mapping[upos]
         wikibase_language = mappings.lang_mapping[language_wikicode]
-        newlexeme = lexeme(lexical_category=wikibase_pos, language=wikibase_language)
+        newlexeme = xwbi.wbi.lexeme.new(lexical_category=wikibase_pos, language=wikibase_language)
 
         newlexeme.lemmas.set(language=language_wikicode, value=lemma)
-        claim = String(prop_nr='P6', value="TODO") # TODO: lemma ID??
-        newlexeme.claims.add(claim)
-        for sense_id in upload_data[lemma]:
+        # claim = String(prop_nr='P7', value="TODO") # TODO: lemma ID??
+        # newlexeme.claims.add(claim)
+        for sense_id in upload_data[lempos]:
+            print(sense_id)
             newsense = Sense()
-            newsense.glosses.set(language=language_wikicode, value=upload_data[lemma][sense_id]['definition'])
-            claim = String(prop_nr='P6', value=sense_id)
+            newsense.glosses.set(language=language_wikicode, value=upload_data[lempos][sense_id]['definition'])
+            claim = String(prop_nr='P7', value=sense_id)
             newsense.claims.add(claim)
-            newlexeme.add(newsense)
+            newlexeme.senses.add(newsense)
 
-        print(lexeme.get_json)
+        done = False
+        while not done:
+            try:
+                newlexeme.write()
+                done = True
+                print("Upload successful.")
+                time.sleep(0.2)
+            except Exception as ex:
+                if "404 Client Error" in str(ex):
+                    print('Got 404 response from wikibase, will wait and try again...')
+                    time.sleep(10)
+                else:
+                    print('Unexpected error:\n' + str(ex))
+                    sys.exit()
+
+        with open(f'sense_inventories/{language_wikicode}-lexeme-mapping.csv', 'a') as mappingcsv:
+            mappingcsv.write(lempos + '\t' + newlexeme.id + '\n')
+        print('Finished processing ' + newlexeme.id)
+
+
+
+
 
 
 
